4608888c1cd08fb9c997454903a4158dc58b90b8
Jonathan Rice <freedomfinishline7@gmail.com>
2023-05-25 12:00:20 -0500

    Added files to repo from local folder

---
+++ jenkins_server/variables.tf
@@ -0,0 +1,17 @@
+variable "jon-clarusway" {
+  default = "clarus"
+}
+
+variable "instancetype" {
+  default = "t3a.medium"
+}
+variable "tag" {
+  default = "Jenkins_Server_jon"
+}
+variable "jenkins-sg" {
+  default = "jenkins-server-sec-gr-208"
+}
+
+variable "user" {
+  default = "jon"
+}
